f2790c6228e21ac2df36f0bf115dee43c5624025
Marcos Malveira <163179361+Perruni@users.noreply.github.com>
2024-10-18 20:38:01 -0300

    Add:Migracion de evento

--- database/migrations/2024_10_16_143723_create_eventos_table.php
+++ database/migrations/2024_10_16_143723_create_eventos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('nombreEvento');
             $table->string('descripcion');
             $table->dateTime('fecha');
